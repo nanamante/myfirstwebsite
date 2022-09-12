71033baf6cd44b7f2004bf170182762ad754c5d7
nanamante <nanamante1@icloud.com>
2022-09-12 17:25:26 +0100

    first push to git hub

---
+++ string.java
@@ -0,0 +1,6 @@
+public class string{
+    public static void main(String args[]){
+        int n1=6500,n2=2000,sum;
+        sum=n1+n2;
+        System.out.println("The sum of numbers is : " + sum);}
+    }
---
+++ sumofnumbers.java
@@ -0,0 +1,7 @@
+public class sumofnumbers{
+    public static void main(String args[]){
+        int n1=50,n2=50,sum;
+        sum=n1+n2;
+        System.out.println("The sum of numbers is : " + sum);
+    }   
+}
